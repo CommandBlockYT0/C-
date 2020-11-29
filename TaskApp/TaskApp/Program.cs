ad3feb70ec2889e3d3a4dc876a0eeac8145f4fd5
KVV <44994888+CommandBlockYT0@users.noreply.github.com>
2020-11-29 19:33:26 +0300

    First Task

---
+++ TaskApp/TaskApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Task01
+{
+    class Box
+    {
+        int num;
+        public int Num { get { return Num; } }
+        public Box(int num) => this.num = num;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task<int> task = new Task<int>(delegate ()
+            {
+                int sum = 0;
+                for (int i = 1; i <= 100; i++)
+                {
+                    sum += i;
+                }
+                return sum;
+            });
+            task.Start();
+            Console.WriteLine(task.Result);
+
+        }
+    }
+}
